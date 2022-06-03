1b5063366a946e043ec9256ad6317ac167367341
Gustavo Mendes <gusttavo.me@outlook.com>
2022-06-03 10:46:43 -0300

    alioth: Use raven fp
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -31,4 +31,7 @@ PRODUCT_MODEL := M2012K11AG
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := google/redfin/redfin:12/SP2A.220505.002/8353555:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220505.002 8353555 release-keys"
